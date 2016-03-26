8f9e430dbb2916110557b83fcb90b6cb1bccdab3
BBK-PiJ-2015-10 <yasserpo@hotmail.com>
2016-03-26 17:20:57 +0000

    Developed a first solution to the Explorer method. Need to optimize it. Mostly, on improving its back track procedure.

---
+++ src/gui/ROOT.java
@@ -0,0 +1,11 @@
+package gui;
+
+interface ROOT {
+    //final static String ROOT = "/Users/keith/Courses/PiJ/repo/cw-temple/res/";
+	
+	//C:\Users\YasserAlejandro\SDP\Ongoing\cw-temple\res
+	
+    //final static String ROOT = "/Users/YasserAlejandro/PIJ/cw-temple/res/";
+	final static String ROOT = "/Users/YasserAlejandro/SDP/Ongoing/cw-temple/res/";
+
+}
